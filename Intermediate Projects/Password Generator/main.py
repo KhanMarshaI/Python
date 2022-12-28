dbc9169fdffbe74910e142a9aab8511a79279029
KhanMarshaI <iamtaha05@gmail.com>
2022-12-28 23:02:56 +0500

    Update the program from a CLI to GUI. Tweaked the code, also added an input check.

--- Intermediate Projects/Password Generator/main.py
+++ Intermediate Projects/Password Generator/main.py
@@ -1,21 +1,45 @@
 import random
+import PySimpleGUI as gui
+
+
 class Generator:
     List = []
-
-    def __init__(self):
-        for i in range(lengthOfPassword//2):
+    def __init__(self, length):
+        for i in range(length//2):
             tmp = chr(random.randint(65, 122))
             Generator.List.append(tmp)
-        for j in range(lengthOfPassword//2):
+        for j in range(length//2):
             tmp_digits = chr(random.randint(48,57))
             Generator.List.append(tmp_digits)
 
     def log(self):
         random.shuffle(Generator.List)
         tmp = ''.join([str(item) for item in Generator.List])
-        print(tmp)
+        Generator.List.clear()
+        return tmp
+
+# GUI
+gui.theme('Dark')
+layout = [
+        [gui.Text('Insert the length of password in digits:')],
+        [gui.Input(key = '-INPUT-')],
+        [gui.Text(size=(40,1), key='-OUTPUT-')],
+        [gui.Button('Generate'), gui.Button('Quit')]
+]
+
+window = gui.Window('Password Generator', layout)
 
 
-lengthOfPassword = int(input('Insert the length of the password: '))
-s = Generator()
-s.log()
+while True:
+    event, value = window.read()
+    if event == gui.WINDOW_CLOSED or event == 'Quit':
+        break
+
+    try:
+        tmp = int(value['-INPUT-'])
+        password = Generator(tmp)
+        window['-OUTPUT-'].update('Password = ' + password.log())
+    except:
+        gui.popup('Invalid input.')
+
+
