f032f581ac50cfef6dd17c43cea44e761bd20e61
KhanMarshaI <iamtaha05@gmail.com>
2023-01-04 17:24:24 +0500

    Add the functionality of operations with the use of eval(). Allow only keypad input to mitigate the dangers of eval().

--- Intermediate Projects/Calculator/main.py
+++ Intermediate Projects/Calculator/main.py
@@ -1,7 +1,7 @@
 import PySimpleGUI as gui
 
 gui.theme('DarkBlack1')
-layout = [[gui.Input(default_text='',size=(23,1), background_color='White', key='_input_')],
+layout = [[gui.Input(default_text='',size=(23,1), background_color='White', key='_input_', enable_events=True)],
           [gui.Text(size=(20,1), key='_output_'), gui.Button('=')],
           [gui.Button('1'),gui.Button('2'), gui.Button('3'), gui.Button('/')],
           [gui.Button('4'),gui.Button('5'),gui.Button('6'), gui.Button('*')],
@@ -16,11 +16,16 @@ while True:
     event, value = window.read()
     if event == gui.WINDOW_CLOSED:
         break
+    if event == '=':
+        window['_output_'].update(eval(value['_input_'])) # eval can be dangerous as it treats everything like a command
     if event == 'C':
         userInput = ''
     if event in '0123456789+-/*.':
         userInput += event
-
+        # if last char entered not a digit
+    if event == '_input_' and len(value['_input_']) and value['_input_'][-1] not in ('0123456789+-/*.'):
+        # delete last char from input
+        window['_input_'].update(value['_input_'][:-1])
     window['_input_'].update(userInput)
 
 
